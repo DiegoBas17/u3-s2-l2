16afddb210ceea30ab0da12bf2609507648e1e98
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-07-02 14:10:07 +0200

    trasformato app e mynav in hooks

--- src/component/MyNav.jsx
+++ src/component/MyNav.jsx
@@ -1,58 +1,45 @@
-import { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-class MyNav extends Component {
-  render() {
-    return (
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container fluid>
-          <Navbar.Brand href="#home">EpicBooks</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">About</Nav.Link>
-              <Nav.Link href="#link">Browse</Nav.Link>
-              <NavDropdown title="Categoria" id="basic-nav-dropdown">
-                <NavDropdown.Item
-                  onClick={() => this.props.changeCategory("Fantasy")}
-                >
-                  Fantasy
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item
-                  onClick={() => this.props.changeCategory("History")}
-                >
-                  History
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item
-                  onClick={() => this.props.changeCategory("Horror")}
-                >
-                  Horror
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item
-                  onClick={() => this.props.changeCategory("Romance")}
-                >
-                  Romance
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item
-                  onClick={() => this.props.changeCategory("Scifi")}
-                >
-                  Scifi
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-}
+const MyNav = ({ changeCategory }) => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#home">EpicBooks</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Browse</Nav.Link>
+            <NavDropdown title="Categoria" id="basic-nav-dropdown">
+              <NavDropdown.Item onClick={() => changeCategory("Fantasy")}>
+                Fantasy
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={() => changeCategory("History")}>
+                History
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={() => changeCategory("Horror")}>
+                Horror
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={() => changeCategory("Romance")}>
+                Romance
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={() => changeCategory("Scifi")}>
+                Scifi
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default MyNav;
