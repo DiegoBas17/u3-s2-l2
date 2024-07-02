d351caaf282ed217b238bee5c4ce0683a0c2d873
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-07-02 14:24:54 +0200

    add hooks a welcome e booklist

--- src/component/BookList.jsx
+++ src/component/BookList.jsx
@@ -1,64 +1,56 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Container, Form, InputGroup, Row, Col } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 import CommentArea from "./CommentArea";
 
-class BookList extends Component {
-  state = {
-    searchInput: "",
-    idLibroSelezionato: "",
+const BookList = ({ libriDaVisualizzare }) => {
+  const [searchInput, setSearchInput] = useState("");
+  const [idLibroSelezionato, setIdLibroSelezionato] = useState("");
+
+  const cardSelezionata = (idLibroSelezionato) => {
+    setIdLibroSelezionato(idLibroSelezionato);
   };
 
-  cardSelezionata = (idLibroSelezionato) => {
-    this.setState({
-      idLibroSelezionato: idLibroSelezionato,
-    });
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
   };
 
-  handleChange = (e) => {
-    this.setState({ searchInput: e.target.value });
-  };
-
-  render() {
-    return (
-      <Container>
-        <InputGroup className="mb-3">
-          <Form.Control
-            type="text"
-            placeholder="Cerca"
-            value={this.state.searchInput}
-            onChange={this.handleChange}
-          />
-        </InputGroup>
-        <Row className="g-2">
-          <Col md="8">
-            <Row>
-              {this.props.libriDaVisualizzare
-                .filter((libro) =>
-                  libro.title
-                    .toLowerCase()
-                    .includes(this.state.searchInput.toLowerCase())
-                )
-                .map((libro, index) => (
-                  <Col md="6" key={index}>
-                    <SingleBook
-                      libro={libro}
-                      cardSelezionata={this.cardSelezionata}
-                      isSelected={this.state.idLibroSelezionato === libro.asin}
-                    />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-          <Col md="4">
-            {this.state.idLibroSelezionato && (
-              <CommentArea idLibro={this.state.idLibroSelezionato} />
-            )}
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <InputGroup className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Cerca"
+          value={searchInput}
+          onChange={handleChange}
+        />
+      </InputGroup>
+      <Row className="g-2">
+        <Col md="8">
+          <Row>
+            {libriDaVisualizzare
+              .filter((libro) =>
+                libro.title.toLowerCase().includes(searchInput.toLowerCase())
+              )
+              .map((libro, index) => (
+                <Col md="6" key={index}>
+                  <SingleBook
+                    libro={libro}
+                    cardSelezionata={cardSelezionata}
+                    isSelected={idLibroSelezionato === libro.asin}
+                  />
+                </Col>
+              ))}
+          </Row>
+        </Col>
+        <Col md="4">
+          {this.state.idLibroSelezionato && (
+            <CommentArea idLibro={idLibroSelezionato} />
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default BookList;
